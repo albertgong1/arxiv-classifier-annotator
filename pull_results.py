6c1fbe3eafadbda41313e29f851216e15cb31060
Albert Gong <ag2435@cornell.edu>
2025-04-03 15:39:11 -0400

    add timestamp to saved results file

--- pull_results.py
+++ pull_results.py
@@ -6,6 +6,8 @@ python pull_results.py
 ```
 """
 
+import os
+from datetime import datetime
 import pandas as pd
 from utils import get_firestore, MODERATOR_RESULTS_COLLECTION
 
@@ -19,4 +21,9 @@ mod_results_docs = mod_results_ref.stream()
 mod_results_df = pd.DataFrame([doc.to_dict() for doc in mod_results_docs])
 
 # save the dataframe to a csv file
-mod_results_df.to_csv("mod_results.csv", index=False)
+results_dir = "data/results"
+os.makedirs(results_dir, exist_ok=True)
+save_path = os.path.join(
+    results_dir, f"mod_results-{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}.csv"
+)
+mod_results_df.to_csv(save_path, index=False)
